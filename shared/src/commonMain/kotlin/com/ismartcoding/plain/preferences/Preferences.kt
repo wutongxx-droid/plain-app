be361695f14013c0c8764e2595b810964ca6e7f1
test <test@test.com>
2026-06-08 17:12:20 +0800

    Fix: use hardcoded default STUN servers in Preferences.kt
    DEFAULT_STUN_SERVERS is in app module, not accessible from shared module

--- shared/src/commonMain/kotlin/com/ismartcoding/plain/preferences/Preferences.kt
+++ shared/src/commonMain/kotlin/com/ismartcoding/plain/preferences/Preferences.kt
@@ -196,7 +196,7 @@ object ScreenMirrorQualityPreference : BasePreference<String>() {
 }
 
 object StunServersPreference : BasePreference<String>() {
-    override val default = DEFAULT_STUN_SERVERS.joinToString(", ")
+    override val default = "stun:stun.l.google.com:19302, stun:stun1.l.google.com:19302, stun:stun2.l.google.com:19302, stun:stun3.l.google.com:19302, stun:stun4.l.google.com:19302, stun:global.stun.twilio.com:3478"
     override val key = stringPreferencesKey("stun_servers")
 }
 
